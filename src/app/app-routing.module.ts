5289a66cc86b4f1b43ba501dfeeabff927db0f47
Hiro? <brenofprocopio@gmail.com>
2022-08-09 08:48:19 -0300

    v0.4
    + redirecionamento de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch:'full', redirectTo:'cursos' // quando iniciar o site será redirecionado automaticamente a cursos
+  },
+  {
+    path: 'cursos', // caminho para modulo curso
+    loadChildren: ()=> import('./cursos/cursos.module').then(m => m.CursosModule) // importação do modulo curso e apelidando
+  },
+  {
+    path: 'alunos', // caminho para modulo curso
+    loadChildren: ()=> import('./alunos/alunos.module').then(m => m.AlunosModule) // importação do modulo curso e apelidando
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cursos/cursos-routing.module.ts
+++ src/app/cursos/cursos-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursosComponent } from './cursos/cursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CursosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
